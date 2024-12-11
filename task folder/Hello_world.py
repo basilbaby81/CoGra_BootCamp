1bca3855bb122644586e7b58b8c1b46d12b5b269
basilbaby81 <40324983+basilbaby81@users.noreply.github.com>
2024-12-11 18:17:49 +0000

    Create Hello_world.py

---
+++ task folder/Hello_world.py
@@ -0,0 +1,17 @@
+# *****Pseudocode***
+# get user's name as inupt
+# Print the name
+# get user's age as inupt
+# Print the age
+# print "Hello word "
+
+# getting name from user and printing
+name = input("Enter your name: ")
+print(f"Your name is {name}")
+
+# getting age from user and printing
+age = input("Enter your age: ")
+print(f"Your age is {age}")
+
+# Printing Hello world!
+print("Hello World!")
